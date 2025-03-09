f2d04d5b6b3e500e3b8d6041f25e030aaada148a
Jimmy2004S <tucorreo@example.com>
2025-03-09 07:24:18 -0500

    Middleware para validar autentificacion de usuarios

--- src/Interfaces/http/middlewares/AuthMiddleware.php
+++ src/Interfaces/http/middlewares/AuthMiddleware.php
@@ -3,8 +3,11 @@
 namespace Src\Interfaces\http\middlewares;
 
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Src\Infraestructure\Api\AuthService as ApiAuthService;
+use Src\Infraestructure\Api\LogService;
 use Src\Infrastructure\Api\AuthService;
 
 class AuthMiddleware
@@ -12,19 +15,34 @@ class AuthMiddleware
     public function handle(Request $request, Closure $next)
     {
 
-        $token = $request->header('Authorization');
-        if (!$token) {
-            return response()->json(['message' => 'Token not provided'], 401);
+        try {
+
+            $token = $request->header('Authorization');
+            if (!$token) {
+                return response()->json(['message' => 'Token not provided'], 401);
+            }
+
+            $response = ApiAuthService::getUserFromToken($token);
+
+            // Extraer el usuario del JSON correctamente
+            $data = $response->json();
+
+
+            if (!$data['success']) {
+                return response()->json(['message' => 'Problem in the external api'], 500);
+            }
+
+            $user = $data['user'];
+
+            // Agregar el usuario autenticado al request
+            $request->merge(['user' =>  $user]);
+            return $next($request);
+        } catch (Exception $e) {
+            LogService::store('transaction', 'get', $e->getMessage());
+            return response()->json(
+                ['error' => 'Error authenticating user'],
+                401
+            );
         }
-
-        $response = AuthService::getUserFromToken($token);
-
-        if (!$response->failed()) {
-            return response()->json(['message' => 'Invalid token'], 401);
-        }
-
-        $request->merge(['auth_user' => $response->user]);
-
-        return $next($request);
     }
 }
